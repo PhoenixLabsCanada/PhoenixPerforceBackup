db8da63ade2e3bd0a82c178893dbf9945d4874ae
Jesse Houston <jhouston@phx-perforce.hq.phxlabs.net>
2015-11-25 18:11:41 -0800

    Minor fixed (sorry for the bad changelist description)

--- getNewVersion.sh
+++ getNewVersion.sh
@@ -4,5 +4,7 @@ cp ./config.sh /tmp/config.sh.phoenix
 cp ./getNewVersion.sh /tmp/getNewVersion.sh
 /usr/bin/git fetch
 /usr/bin/git reset --hard origin/master
+chmod +x ./*.sh
+
 mv /tmp/getNewVersion.sh ./getNewVersion.sh
 mv /tmp/config.sh.phoenix ./config.sh
